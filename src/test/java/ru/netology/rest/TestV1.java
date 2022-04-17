85a85375aef63847e4ce5be4144cd8233dcb5e2a
Naan <naanko2021@gmail.com>
2022-04-17 20:06:31 +0300

    first commit

---
+++ src/test/java/ru/netology/rest/TestV1.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class TestV1 {
+    @Test
+    void shouldDemo() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
